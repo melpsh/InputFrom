b965c62d19d0611fa06daaf1bd1fa43c7267d8e7
Melina Pashna <melinaapashna@gmail.com>
2024-08-17 21:18:48 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import InputFrom from './components/InputForm/InputFrom'
+
+function App() {
+
+  return (
+    <>
+      <InputFrom />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InputForm/InputFrom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputFrom = () => {
+  return <div>InputFrom</div>;
+};
+
+export default InputFrom;
